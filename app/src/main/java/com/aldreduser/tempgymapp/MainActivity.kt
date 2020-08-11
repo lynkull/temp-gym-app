95d1508b9fe7f4bec98b81eb10e0ade4fbc27014
Ed Cres <crespoed07@gmail.com>
2020-08-11 01:19:22 -0400

    iteration through workouts is set up

--- app/src/main/java/com/aldreduser/tempgymapp/MainActivity.kt
+++ app/src/main/java/com/aldreduser/tempgymapp/MainActivity.kt
@@ -8,6 +8,12 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    private val upperFrontBodyWorkouts = listOf<String>("Incline Dumbbell", "Decline Dumbbell", "Lower Abbs", "Tricep Pulldown", "Bicep Curl")
+    private val upperBackBodyWorkouts = listOf<String>("Lat Pulldown", "Back Pullback", "Shoulder Raises", "Upper Abbs")
+    private val lowerBodyWorkouts = listOf<String>("Leg Press", "Out Calf Raises", "In Calf Raises", "Leg Extension")
+
+    private val workoutCategories = listOf<List<String>>(upperFrontBodyWorkouts, upperBackBodyWorkouts, lowerBodyWorkouts)
+
     private var inclineDumbbellName:String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    //todo: button to save all
 
 
     fun loadAllData() {
@@ -29,26 +36,39 @@ class MainActivity : AppCompatActivity() {
     fun saveAllData() {
         //save all the data
 
-        //loop through the categories //todo: NOTICE: assumes everything has 3 categories
+        //loop through the categories
         //loop through the workouts (put them in lists to loop through them)
         //loop though the sets //todo: NOTICE: assumes everything has 3 sets
 
 
 
-        for (i in 1..5){
-            println(i)
-        }
+        for (item in 1..workoutCategories.size) {
+            //for each category
+            //this should do 3 loops, but it might be wrong (ie. not 3)
 
-        val repsSP = this.getPreferences(Context.MODE_PRIVATE)
-        with(repsSP.edit()) {
-            putString("workout name", /*reps*/)
-            commit()
-        }
+            var category:Int = 0
 
-        val weightSP = this.getPreferences(Context.MODE_PRIVATE)
-        with(weightSP.edit()) {
-            putString("workout name", /*weight*/)
-            commit()
+            for (item in 1..workoutCategories[category].size) {
+                //for each workout
+                //for each workout in the category
+                category++
+
+                for (i in 1..3) {
+                    //for each set
+                    val repsSP = this.getPreferences(Context.MODE_PRIVATE)
+                    with(repsSP.edit()) {
+                        putString("workout name", /*reps*/)
+                        commit()
+                    }
+                    val weightSP = this.getPreferences(Context.MODE_PRIVATE)
+                    with(weightSP.edit()) {
+                        putString("workout name", /*weight*/)
+                        commit()
+                    }
+                }
+            }
+            
+
         }
 
     }
